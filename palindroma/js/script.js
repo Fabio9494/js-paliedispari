151f17562d01a4a9540a0b23fad7e9a1345d30fc
Fabio9494 <fabio.casoria94@gmail.com>
2023-11-28 19:55:54 +0100

    added changes to find new_word

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,13 +4,10 @@ console.log(palindroma(parola));
 //FUNZIONI
 function palindroma(word){
     let new_word;
-    for(i=0;i<word.length;i++){
-        new_word = word.split("").reverse("").join("");
-    }
+    new_word = word.split("").reverse("").join("");
     if (word==new_word){
         return "LA PAROLA INSERITA E' PALINDROMA"
     }
-
     return "LA PAROLA INSERITA NON E' PALINDROMA"
 }
 
