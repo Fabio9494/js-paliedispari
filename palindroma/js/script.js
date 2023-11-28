72ea2bb28f84cab182f41f51968c18d21a6ede92
Fabio9494 <fabio.casoria94@gmail.com>
2023-11-28 14:19:58 +0100

    add palindroma exercise

---
+++ palindroma/js/script.js
@@ -0,0 +1,16 @@
+let parola = prompt("INSERISCI PAROLA").toLowerCase();
+console.log(palindroma(parola));
+
+//FUNZIONI
+function palindroma(word){
+    let new_word;
+    for(i=0;i<word.length;i++){
+        new_word = word.split("").reverse("").join("");
+    }
+    if (word==new_word){
+        return "LA PAROLA INSERITA E' PALINDROMA"
+    }
+
+    return "LA PAROLA INSERITA NON E' PALINDROMA"
+}
+
